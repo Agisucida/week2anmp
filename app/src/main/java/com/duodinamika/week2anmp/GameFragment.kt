f24b1f71fa4771711720b2d062429818a69bdf54
Agisucida <s160920007@student.ubaya.ac.id>
2023-09-07 11:30:05 +0700

    first commit

---
+++ app/src/main/java/com/duodinamika/week2anmp/GameFragment.kt
@@ -0,0 +1,43 @@
+package com.duodinamika.week2anmp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.Navigation
+import kotlin.random.Random
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        arguments?.let {
+            val playerName =
+                GameFragmentArgs.fromBundle(requireArguments()).playerName
+            val txtTurn = view.findViewById<TextView>(R.id.txtTurn)
+            txtTurn.text = "$playerName's Turn"
+        }
+    }
+
+}
